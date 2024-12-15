40972e5bb739ef78cfcf2eee68d358dce2f00f6f
utkuerkul <152040484+utkuerkul@users.noreply.github.com>
2024-12-15 15:14:08 +0300

    Delete BTK Hackaton directory

--- BTK Hackaton/hackhaton_btk/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ExamModel, ResultModel, DailyReportModelAYT, DailyReportModelTYT
-from .models import APICall
-
-admin.site.register(ExamModel)
-admin.site.register(ResultModel)
-admin.site.register(DailyReportModelAYT)
-admin.site.register(DailyReportModelTYT)
-admin.site.register(APICall)
